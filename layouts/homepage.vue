6e960570cc254184a42e6236b483a5f4a5d233c2
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-02-13 22:35:44 +0000

    costum layout for homepage

---
+++ layouts/homepage.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <!-- Loader -->
+    <Loader />
+    <!-- Loader -->
+    <!-- Navbar Start -->
+    <NavBar />
+    <!-- Navbar End -->
+
+    <!-- Page Content Start -->
+    <Nuxt />
+    <!-- Page Content End -->
+
+    <!-- Footer Start -->
+    <Footer />
+    <!-- Footer End -->
+
+    <!-- To up btn Start -->
+    <ToUp />
+    <!-- To up btn End -->
+
+    <!-- Javascript -->
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+    <script src="/js/jquery.easing.min.js"></script>
+    <script src="/js/scrollspy.min.js"></script>
+
+    <!-- Magnific Js -->
+    <script src="/js/isotope.js"></script>
+    <script src="/js/jquery.magnific-popup.min.js"></script>
+    <script src="/js/magnific.init.js"></script>
+
+    <!-- Slider Js -->
+    <script src="/js/owl.carousel.min.js "></script>
+    <script src="/js/owl.init.js "></script>
+
+    <!-- Contact -->
+    <script src="/js/contact.js"></script>
+
+    <!-- Counter init -->
+    <script src="/js/counter.init.js"></script>
+
+    <!-- Feather icon -->
+    <script src="/js/feather.min.js"></script>
+
+    <!-- Typed -->
+    <script src="/js/typed.js"></script>
+    <script src="/js/typed.init.js"></script>
+
+    <!-- Flickity Js -->
+    <script src="/js/flickity.pkgd.min.js"></script>
+
+    <!-- Main Js -->
+    <script src="/js/app.js"></script>
+  </div>
+</template>
+
+<script >
+import Vue from "vue";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import ToUp from "../components/ToUp";
+import Loader from "../components/Loader";
+
+export default Vue.extend({
+  components: {
+    NavBar,
+    Footer,
+    ToUp,
+    Loader,
+  },
+});
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -514,7 +514,9 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  layout: 'homepage',
+})
 </script>
 
 <style>
