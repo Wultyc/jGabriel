8b2429bcec4e3cd1429419dc6735292c28c8d0bb
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-02-13 22:36:06 +0000

    create portfolio page

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,5 +1,221 @@
 <template>
-  
+    <!-- Blog STart -->
+    <section class="section">
+        <div class="container">
+            <div class="row">
+                <ul class="col container-filter list-unstyled categories-filter text-center mb-0" id="filter">
+                    <li class="list-inline-item"><a class="categories border d-block text-dark rounded active" data-filter="*">All</a></li>
+                    <li class="list-inline-item"><a class="categories border d-block text-dark rounded" data-filter=".branding">Branding</a></li>
+                    <li class="list-inline-item"><a class="categories border d-block text-dark rounded" data-filter=".designing">Designing</a></li>
+                    <li class="list-inline-item"><a class="categories border d-block text-dark rounded" data-filter=".photography">Photography</a></li>
+                    <li class="list-inline-item"><a class="categories border d-block text-dark rounded" data-filter=".development">Development</a></li>
+                </ul>
+            </div>
+
+            <div class="row projects-wrapper">
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 branding">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/1.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Iphone mockup</a></h5>
+                                <h6 class="text-light tag mb-0">Branding</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/1.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+                
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 designing">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/2.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Mockup Collection</a></h5>
+                                <h6 class="text-light tag mb-0">Mockup</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/2.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+                
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 photography">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/3.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Abstract images</a></h5>
+                                <h6 class="text-light tag mb-0">Abstract</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/3.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+                
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 development">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/4.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Yellow bg with Books</a></h5>
+                                <h6 class="text-light tag mb-0">Books</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/4.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+                
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 branding">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/5.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Company V-card</a></h5>
+                                <h6 class="text-light tag mb-0">V-card</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/5.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+                
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 branding">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/6.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Mockup box with paints</a></h5>
+                                <h6 class="text-light tag mb-0">Photography</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/6.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 designing">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/7.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Yellow bg with Books</a></h5>
+                                <h6 class="text-light tag mb-0">Books</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/7.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+                
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 development">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/8.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Company V-card</a></h5>
+                                <h6 class="text-light tag mb-0">V-card</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/8.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+                
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 photography">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/9.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Mockup box with paints</a></h5>
+                                <h6 class="text-light tag mb-0">Photography</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/9.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 photography">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/10.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Iphone mockup</a></h5>
+                                <h6 class="text-light tag mb-0">Branding</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/10.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+                
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 designing">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/11.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Mockup Collection</a></h5>
+                                <h6 class="text-light tag mb-0">Mockup</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/11.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+                
+                <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2 designing">
+                    <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                        <div class="card-body p-0">
+                            <img src="images/portfolio/3.jpg" class="img-fluid" alt="work-image">
+                            <div class="overlay-work bg-dark"></div>
+                            <div class="content">
+                                <h5 class="mb-0"><a href="page-portfolio-detail.html" class="text-white title">Abstract images</a></h5>
+                                <h6 class="text-light tag mb-0">Abstract</h6>
+                            </div>
+                            <div class="icons text-center">
+                                <a href="images/portfolio/3.jpg" class="text-primary work-icon bg-white d-inline-block rounded-pill mfp-image"><i data-feather="camera" class="fea icon-sm"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end col-->
+
+                <div class="col-12 mt-4 pt-2">
+                    <div class="text-center">
+                        <a href="javascript:void(0)" class="btn btn btn-primary rounded">See More <i class="mdi mdi-chevron-right"></i></a>
+                    </div>
+                </div><!--end col-->
+
+            </div><!--end row-->
+        </div><!--end container-->
+    </section><!--end section-->
+    <!-- Blog End -->
+
 </template>
 
 <script lang="ts">
