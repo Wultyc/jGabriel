daf249d9ce317e87db93dbcdf6d35983673ae9d8
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-02-13 22:34:04 +0000

    dynamic links and project name from vuex store

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg fixed-top navbar-custom navbar-light sticky">
         <div class="container">
                 <!-- Logo container-->
-                <a class="logo navbar-brand" href="#">Queue</a>
+                <a class="logo navbar-brand" href="#">{{ $store.state.projectName }}</a>
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" 
                 aria-label="Toggle navigation">
@@ -12,23 +12,24 @@
                 <div class="collapse navbar-collapse navigation" id="navbarCollapse">
                     <ul class="navbar-nav navbar-nav-link ml-auto">
                         <li class="nav-item">
-                            <nuxt-link class="nav-link" href="#home" to="/">Home</nuxt-link>
-                        </li><!--end nav item-->
+                            <nuxt-link class="nav-link" to="/">Home</nuxt-link>
+                        </li>
                         <li class="nav-item">
-                            <nuxt-link class="nav-link" href="#about-me" to="/about-me">About Me</nuxt-link>
-                        </li><!--end nav item-->
+                            <nuxt-link class="nav-link" to="/about-me">About Me</nuxt-link>
+                        </li>
                         <li class="nav-item">
-                            <nuxt-link class="nav-link" href="#portfolio" to="/portfolio">Portfolio</nuxt-link>
-                        </li><!--end nav item-->
+                            <nuxt-link class="nav-link" to="/portfolio">Portfolio</nuxt-link>
+                        </li>
                         <li class="nav-item">
-                            <nuxt-link class="nav-link" href="#contact" to="/contact">Contact</nuxt-link>
-                        </li><!--end nav item-->
+                            <nuxt-link class="nav-link" to="/contact">Contact</nuxt-link>
+                        </li>
+                        <li class="nav-item" v-for="link in $store.state.externalPages.filter((l) => l.showOnNavBar)" :key="link.name">
+                            <a class="nav-link" target="_blank" :href="link.url">{{link.name}}</a>
+                        </li>
                     </ul>
 
                     <ul class="top-right text-right list-unstyled list-inline mb-0 mt-2 mt-sm-0 nav-social">
-                        <li class="list-inline-item mr-2"><a href="javascript:void(0)"><i class="mdi mdi-facebook"></i></a></li>
-                        <li class="list-inline-item mr-2"><a href="javascript:void(0)"><i class="mdi mdi-twitter"></i></a></li>
-                        <li class="list-inline-item"><a href="javascript:void(0)"><i class="mdi mdi-instagram"></i></a></li>
+                        <li class="list-inline-item" v-for="link in $store.state.links.filter((l) => l.showOnNavBar)" :key="link.name"><a target="_blank" :href="link.url"><i :class="link.icon"></i></a></li>
                     </ul>
                     
                 </div> 
