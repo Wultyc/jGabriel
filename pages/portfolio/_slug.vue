5732ea69c81c2a1349c7bcb100eb4191c35e9105
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-03-14 00:47:14 +0000

    dummy project

---
+++ pages/portfolio/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section pb-0" id="contact">
+    <div class="container">
+      <article>
+        <nuxt-content :document="project" />
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const project = await $content("projects", params.slug).fetch();
+
+    console.log(project)
+
+    return { project };
+  }
+};
+</script>
