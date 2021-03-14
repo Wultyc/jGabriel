cbc3ca424a228d2b2a2c5fc9ba4ca7f89b081502
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-03-14 00:27:34 +0000

    Example post with nuxt-content

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,8 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxt/content'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
---
+++ pages/articles/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section pb-0" id="contact">
+    <div class="container">
+      <article>
+        <nuxt-content :document="article" />
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content("articles", params.slug).fetch();
+
+    return { article };
+  }
+};
+</script>
