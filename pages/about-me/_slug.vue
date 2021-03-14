d04bdfc0e5cb5442ac3dc753dc5896780a04517d
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-03-14 00:39:16 +0000

    load dummy yaml file

---
+++ pages/about-me/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section pb-0" id="contact">
+    <div class="container">
+      <article>
+        <nuxt-content :document="life" />
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const life = await $content("life", "events").fetch();
+
+    console.log(life)
+
+    return { life };
+  }
+};
+</script>
