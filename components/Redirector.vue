ac9398ee219840eb530f6ffd7ef5201ad878b2b2
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-07-18 02:16:49 +0100

    redirector

---
+++ components/Redirector.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>Hello</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend( {
+    name: "Redirector",
+    props: {
+        routesList: {
+            type: Object,
+            required: true
+        },
+        routeId: {
+            type: String,
+            default: "home"
+        },
+    },
+
+    mounted(){
+        const route:any = this.routesList.routes.find((e:any) => e.id == this.routeId) ?? {url: '/', local: true}
+        if(route.local == true){
+            this.$router.push(route.url)
+        } else {
+            window.location.href = route.url
+        }
+    },
+})
+</script>
+
+<style>
+
+</style>
---
+++ pages/redirect/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <Redirector :routesList="routesList"/>
+</template>
+
+<script>
+import Vue from 'vue'
+import Redirector from "@/components/Redirector.vue"
+
+export default Vue.extend({
+  component: {
+      Redirector
+  },
+
+  async asyncData({ $content, params }) {
+    const routesList = await $content("redirect", "routes").fetch();
+    return { routesList };
+  }
+
+});
+</script>
