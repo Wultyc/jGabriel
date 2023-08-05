bb46fbd6723a07abe13d30bda8107c6aa034ddee
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2023-08-05 20:54:39 +0100

    Add style files to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
 
+import 'style/css/bootstrap.min.css'
+import 'style/css/mklb.css'
+import 'style/css/materialdesignicons.min.css'
+import 'style/css/tiny-slider.css'
+//import 'style/css/style.css' //id="theme-opt"
+//import 'style/css/colors/default.css' //id="color-opt
+import 'style/scss/style.scss'
+
 export const metadata: Metadata = {
   title: "jGabriel",
   description: "My Website",
