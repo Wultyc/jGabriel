4edac509353a8ce00d4550d93a6b622999218234
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2023-08-05 19:16:21 +0100

    Initializing NextJS Project
    Squashed commit of the following:
    
    commit 74e5565e26dcedad7460cedaef4cba12c1e41543
    Author: Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
    Date:   Sat Aug 5 19:15:33 2023 +0100
    
        Configure Typescript to use folder src as base path for imports
    
    commit 525eaf43b824c9bc21fb545c6ea79512a36e2f69
    Author: Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
    Date:   Sat Aug 5 19:11:05 2023 +0100
    
        removing boilerplate
    
    commit d426eaf78de985f4c0e9a3fb725d1cb6c599cdf7
    Author: Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
    Date:   Sat Aug 5 18:17:41 2023 +0100
    
        init NextJS project
    
    commit 93e7d4c2cbf743a211cd9ae20e2484b4f20379fa
    Author: Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
    Date:   Sat Aug 5 18:17:23 2023 +0100
    
        init NextJS project

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "jGabriel",
+  description: "My Website",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
