c9e751d839600db9540dcae70fbca6ca09141dfe
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2023-08-05 22:01:28 +0100

    remove comments with CSS styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import 'style/css/bootstrap.min.css'
 import 'style/css/mklb.css'
 import 'style/css/materialdesignicons.min.css'
 import 'style/css/tiny-slider.css'
-//import 'style/css/style.css' //id="theme-opt"
-//import 'style/css/colors/default.css' //id="color-opt
 import 'style/scss/style.scss'
 
 export const metadata: Metadata = {
