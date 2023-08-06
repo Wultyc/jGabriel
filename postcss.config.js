56df67c54d2023de5d586f0f4b8538fe67420ee8
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2023-08-06 16:11:01 +0100

    fix issues with tailwindcss and postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 //const tailwindcss = require('tailwindcss');
 
-import * as tailwindcss from 'tailwindcss'
-
 module.exports = {
     plugins: [
-        tailwindcss('./tailwind.config.js'), require('autoprefixer')
+        //tailwindcss('./tailwind.config.js'), require('autoprefixer')
+        'tailwindcss',
+        'autoprefixer'
     ]
 };
